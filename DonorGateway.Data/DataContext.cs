d213cddc6811a55c1c150ddcc4477c2620ce5e90
Mark Lawrence <marklawrence72@gmail.com>
2016-07-29 16:46:20 -0500

    ApplicationUser domain object and migrations

---
+++ DonorGateway.Data/DataContext.cs
@@ -0,0 +1,38 @@
+﻿using DonorGateway.Domain;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace DonorGateway.Data
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+            Database.Log = msg => Debug.WriteLine(msg);
+        }
+
+        public static DataContext Create()
+        {
+            return new DataContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Properties<string>().Configure(c => c.HasColumnType("varchar"));
+            builder.Properties<DateTime>().Configure(c => c.HasColumnType("smalldatetime"));
+
+            builder.Entity<ApplicationUser>().ToTable("Users", "Security");
+            builder.Entity<IdentityUserRole>().ToTable("UserRoles", "Security");
+            builder.Entity<IdentityUserClaim>().ToTable("UserClaims", "Security");
+            builder.Entity<IdentityUserLogin>().ToTable("UserLogins", "Security");
+            builder.Entity<IdentityRole>().ToTable("Roles", "Security");
+
+
+        }
+
+    }
+}
