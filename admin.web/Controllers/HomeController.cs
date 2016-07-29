4aad9a32a95504b962c045af59963babacda9076
Mark Lawrence <marklawrence72@gmail.com>
2016-07-29 16:23:15 -0500

    init domain and data class projects

--- admin.web/Controllers/HomeController.cs
+++ admin.web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace admin.web.Controllers
 {
@@ -13,17 +9,8 @@ namespace admin.web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Users()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
