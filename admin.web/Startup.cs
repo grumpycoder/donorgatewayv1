791c4de6a5765467ffd0ab40e79490aba7982329
Mark Lawrence <marklawrence72@gmail.com>
2016-08-19 21:52:11 -0500

    Layout link images update

--- admin.web/Startup.cs
+++ admin.web/Startup.cs
@@ -20,43 +20,43 @@ namespace admin.web
 
         public static async void Seed()
         {
-            var context = new DataContext();
-            var userManager = new ApplicationUserManager(new ApplicationUserStore(context));
-            var roleManager = new ApplicationRoleManager(new ApplicationRoleStore(context));
+            //var context = new DataContext();
+            //var userManager = new ApplicationUserManager(new ApplicationUserStore(context));
+            //var roleManager = new ApplicationRoleManager(new ApplicationRoleStore(context));
 
-            string[] roles = { "admin", "rsvp", "tax", "mailer", "user" };
-            foreach (string role in roles)
-            {
-                if (!context.Roles.Any(r => r.Name == role))
-                {
-                    await roleManager.CreateAsync(new IdentityRole(role));
-                }
-            }
+            //string[] roles = { "admin", "rsvp", "tax", "mailer", "user" };
+            //foreach (string role in roles)
+            //{
+            //    if (!context.Roles.Any(r => r.Name == role))
+            //    {
+            //        await roleManager.CreateAsync(new IdentityRole(role));
+            //    }
+            //}
 
-            var users = roleManager.FindByName("tax-user").Users.ToList();
-            foreach (var user in users)
-            {
-                await userManager.AddToRoleAsync(user.UserId, "tax");
-            }
-            foreach (var user in users)
-            {
-                await userManager.RemoveFromRoleAsync(user.UserId, "tax-user");
-            }
+            //var users = roleManager.FindByName("tax-user").Users.ToList();
+            //foreach (var user in users)
+            //{
+            //    await userManager.AddToRoleAsync(user.UserId, "tax");
+            //}
+            //foreach (var user in users)
+            //{
+            //    await userManager.RemoveFromRoleAsync(user.UserId, "tax-user");
+            //}
 
-            var deletedRole = roleManager.FindByName("tax-user");
-            if (deletedRole != null) roleManager.Delete(deletedRole);
+            //var deletedRole = roleManager.FindByName("tax-user");
+            //if (deletedRole != null) roleManager.Delete(deletedRole);
 
-            if (userManager.FindByName("admin") == null)
-            {
-                var user = new ApplicationUser()
-                {
-                    UserName = "admin",
-                    FullName = "Administrator",
-                    Email = "mark.lawrence@splcenter.org"
-                };
-                userManager.Create(user, "password");
-                await userManager.AddToRoleAsync(user.Id, "admin");
-            }
+            //if (userManager.FindByName("admin") == null)
+            //{
+            //    var user = new ApplicationUser()
+            //    {
+            //        UserName = "admin",
+            //        FullName = "Administrator",
+            //        Email = "mark.lawrence@splcenter.org"
+            //    };
+            //    userManager.Create(user, "password");
+            //    await userManager.AddToRoleAsync(user.Id, "admin");
+            //}
         }
     }
 }
