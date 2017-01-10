cb882b9a7371a3e5c19edee6feca68aaefe096d4
Mark Lawrence <mark.lawrence@southernpoverty.onmicrosoft.com>
2017-01-10 17:42:59 -0600

    fix redirect on webapi paths

--- admin.web/App_Start/Startup.Auth.cs
+++ admin.web/App_Start/Startup.Auth.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
-using web;
 
 namespace admin.web
 {
@@ -21,10 +20,18 @@ namespace admin.web
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
+                
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Auth/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
+                    OnApplyRedirect = ctx =>
+                    {
+                        if(!ctx.Request.Path.StartsWithSegments(new PathString("/api")))
+                        {
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                        }
+                    },
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
@@ -33,5 +40,17 @@ namespace admin.web
                 }
             });
         }
+
+        private static bool IsAjaxRequest(IOwinRequest request)
+        {
+            IReadableStringCollection query = request.Query;
+            if ((query != null) && (query["X-Requested-With"] == "XMLHttpRequest"))
+            {
+                return true;
+            }
+            IHeaderDictionary headers = request.Headers;
+            return ((headers != null) && (headers["X-Requested-With"] == "XMLHttpRequest"));
+        }
+
     }
 }
